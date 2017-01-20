96c98a9f62823414481657ea9eb56c9e69a0cb73
Eon S. Jeon <esjeon@hyunmu.am>
2017-01-20 23:09:59 +0900

    implement getters and setters

--- eatable/eatable.py
+++ eatable/eatable.py
@@ -2,6 +2,9 @@
 import csv
 from typing import Iterable, Any, Union, Dict
 
+ColumnRef = Union[str, int]
+RowIndex = int
+
 class Table:
     """
     A simple in-memory database.
@@ -25,16 +28,14 @@ class Table:
         self.data = [] # type: list[tuple]
         self.column_index = dict(zip(self.header, range(self.width)))
 
-    def __getitem__(self, index: int) -> "Row":
-        # TODO: accept column name
-        return Row(self, index)
+    def __getitem__(self, index: RowIndex) -> "Row":
+        return self.get_row(index)
 
     def __len__(self):
         return len(self.data)
 
-    def __setitem__(self, index: int, data: Iterable) -> None:
-        # TODO: accept column name
-        self.data[index] = tuple(data)
+    def __setitem__(self, index: RowIndex, data: Iterable) -> None:
+        self.set_row(index, data)
 
     def append(self, data: Iterable):
         """
@@ -52,16 +53,39 @@ class Table:
         for row in iterable:
             self.append(row)
 
-    # TODO: rename to get_column_index
-    # TODO: handle both str and int
-    def get_column(self, name: str) -> int:
+    def get_column_index(self, ref: ColumnRef) -> int:
         """
-        Get the index of the column with the given name.
+        Get the index of the column with the given name.abs
+
+        If `int` is given, its value willbe checked and returned if it's a valid index.
+        If any invalid value is given, an exception will be raised.
         """
-        index = self.column_index.get(name)
-        if index is None:
-            raise KeyError("The table has no column named '{}'".format(name))
-        return index
+        if isinstance(ref, int):
+            if 0 <= ref < self.width:
+                return ref
+            raise IndexError("Index({}) out of range([0, {}])".format(0, self.width))
+        elif isinstance(ref, str):
+            index = self.column_index.get(ref)
+            if index is not None:
+                return index
+            raise KeyError("Column('{}') not found".format(ref))
+        raise TypeError("ref must be str or int")
+
+    def get_row(self, index: RowIndex) -> "Row":
+        if not 0 <= index < self.__len__():
+            raise IndexError("index out of range: {}", format(index))
+        return Row(self, index)
+
+    def get_row_data(self, index:RowIndex) -> tuple:
+        return self.data[index]
+
+    def set_row(self, index: RowIndex, data: Iterable):
+        if not 0 <= index < self.__len__():
+            raise IndexError("index out of range: {}", format(index))
+        actual_data = tuple(data)
+        if len(actual_data) != self.width:
+            raise TypeError("width mismatch")
+        self.data[index] = actual_data
 
     # TODO: implement `update(self, columns:Tuple[str], value:Iterable, values:Iterable[Iterable])
 
@@ -71,17 +95,21 @@ class Row:
     A proxy for a row in Table
     """
 
-    def __init__(self, table: Table, index: int) -> None:
+    def __init__(self, table: Table, index: RowIndex) -> None:
         self.table = table
         self.index = index
 
-    def __getitem__(self, key: int) -> Any:
-        return self.table.data[self.index][key]
+    def __getitem__(self, ref: ColumnRef) -> Any:
+        return self.get_data()[self.table.get_column_index(ref)]
 
-    def __setitem__(self, key: int, value: Any) -> None:
-        new_data = list(self.table.data[self.index])
-        new_data[key] = value
-        self.table[self.index] = new_data
+    def __setitem__(self, ref: ColumnRef, value: Any) -> None:
+        data = self.get_data()
+        new_data = list(data)
+        new_data[self.table.get_column_index(ref)] = value
+        self.table.set_row(self.index, new_data)
+
+    def get_data(self) -> tuple:
+        return self.table.get_row_data(self.index)
 
     def update(self, values: tuple = None, changes: Dict[int, Any] = None) -> None:
         # TODO: I think this can be better than this...
