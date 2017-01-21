c24aa7fd33ef8d50aa9a8b4bf54671e10a3488ea
Eon S. Jeon <esjeon@hyunmu.am>
2017-01-21 18:28:24 +0900

    add test_usecase.py
    testing practical usecases

---
+++ tests/test_usecase.py
@@ -0,0 +1,21 @@
+
+import unittest
+from eatable import Table
+
+class UseCaseTestCase(unittest.TestCase):
+    """Test some practical usecases"""
+    def setUp(self):
+        self.people = Table.from_csv_file("tests/people.csv")
+
+    def test_people_by_city(self):
+        result = self.people.select('name', where=lambda row:(
+            row['city'] == 'Waterloo'
+        ))
+        self.assertEqual(result.width, 1)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0]['name'], 'Jack')
+        self.assertEqual(result[1]['name'], 'Frank')
+
+
+if __name__ == '__main__':
+    unittest.main()
